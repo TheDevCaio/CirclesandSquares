98a73b5e9a204459c2d8b988107f7085d05beb90
Caio Dev <caioinnocencio@gmail.com>
2025-02-10 12:57:29 -0300

    Add LoadScreen and Start to the home page and use Effect to decide which to run.

---
+++ circlesandsquares/src/pages/index.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { useEffect, useState } from "react";
+import { Container } from "../components/Global";
+import LoadingScreen from "../components/Loading";
+import Start from "../components/Start";
+
+
+export default function Home() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 3000); 
+    return () => clearTimeout(timer);
+  }, []);
+
+  return (
+    <Container>
+      {loading ? <LoadingScreen /> : <Start/>}
+    </Container>
+  );
+}
